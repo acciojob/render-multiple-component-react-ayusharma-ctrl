d16e0b7ff54cfc15e3e1d91a5b8ee7d11421db62
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-11 22:07:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p data-ns-test="project-name"> Project Name </p>
+      <p data-ns-test="project-description"> This is a description. </p>
+    </div>
   )
 }
-
 
 export default App;
